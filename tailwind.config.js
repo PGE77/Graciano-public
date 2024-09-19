6f7268d1c70d210d09db584a31274ccd91ce397e
Petr Gargulák <gargulakpetr@seznam.cz>
2024-09-19 19:04:38 +0200

    další kommit

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,7 +14,7 @@ module.exports = {
       darkYellow:'#FFEF2E',
       black: '#000000',
       lightBlack:'rgba(0, 0, 0, 0.5)',
-      backgroundGrey:'#EDEDED',
+      lightGrey:'#EDEDED',
      
 
     
@@ -25,15 +25,20 @@ module.exports = {
       sm: '13px',
       md: '14px',
       base: '16px',
-      '1xl': '20px',
-      '2xl': '26px',
+      '1xl': '18px',
+      '2xl': '24px',
       '4xl': '26px',
-      '5xl': '42px',
+      '5xl': '40px',
       '6xl': '117px',
 
     },
     extend:
     {
+      height: {
+      hxl: '638px',
+      hil:'700px',
+        
+      },
       backgroundImage: {
         blackGradient: 'radial-gradient(ellipse farthest-side at center bottom, #D8D8D8 0%, #E2DFDF 0%, #FFFFFF 0%, #FFFFFF 0%, #F3F3F2 60%, #FFFFFF 80%)',
       },
